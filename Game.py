5a9627dccc32ab40ef8836df724e7cca7c848404
Quantsol <35894969+Quantsol@users.noreply.github.com>
2021-04-03 19:01:17 -0400

    working on different classes Areas and Character.

---
+++ Game.py
@@ -0,0 +1,18 @@
+#welcome to my txt based game
+
+
+class Areas:
+
+    def __init__(self):
+        pass
+
+class Character:
+
+    def __init__(self):
+        self.name = name
+
+    def player(self):
+        pass
+
+
+

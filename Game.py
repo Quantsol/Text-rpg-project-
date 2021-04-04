16f09815ad98a2918c95b39dab36e435d02d9ac2
Quantsol <35894969+Quantsol@users.noreply.github.com>
2021-04-03 20:17:32 -0400

    Made a menu function.

--- Game.py
+++ Game.py
@@ -1,18 +1,22 @@
-#welcome to my txt based game
+
+def menu():
+
+    print(" welcome to the first version of Spectra , a text based RPG game ")
+
+    Start_menu = input(str("what would you like to do : Start or Exit? "))
+
+    if Start_menu == "start":
+        print("Game start")
+    else:
+        print("goodbye")
 
 
-class Areas:
 
-    def __init__(self):
-        pass
 
-class Character:
 
-    def __init__(self):
-        self.name = name
 
-    def player(self):
-        pass
+
+
 
 
 
